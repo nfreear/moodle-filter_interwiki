e669001ab850061719d32229e061d827f139ee4e
Nick Freear <nfreear@yahoo.co.uk>
2011-04-15 15:09:17 +0100

    Moodle 2 language pack - Interwiki filter.

---
+++ lang/en/filter_interwiki.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Strings for component 'filter_interwiki', language 'en'.
+ *
+ * @package   filter_interwiki
+ * @copyright (c) 2011 Nicholas Freear {@link http://freear.org.uk}.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ */
+$string['filtername'] = 'Interwiki';
+
+$string['prefix']  = 'Prefix';
+$string['prefhelp']= 'lowercase letters';
+$string['uri']     = 'Link pattern';
+$string['urihelp'] = "$1 replaces ' ' with '_', $2 replaces ' ' with '+'... to $5 ''";
+$string['onhost']  = 'on {$a}';
